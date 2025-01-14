9d9eb34e89ea8dcf168ccb8a926e90a438282d82
Akayy <techakayy@gmail.com>
2025-01-14 12:51:03 +1100

    Updates, navbar refactor and head-and-meta.ts, eslint flat config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,4 @@
 import { EventEmitter } from 'events'
-// Increasing the maxListeners from default 10 to 15 as there are more than 10 vite plugins (mostly vite/astro) ones using fswatcher
-EventEmitter.defaultMaxListeners = 15
 
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'astro/config'
@@ -18,11 +16,15 @@ import { PrimeVueResolver } from '@primevue/auto-import-resolver'
 import AutoImportAPIs from 'unplugin-auto-import/astro'
 import Unocss from 'unocss/vite'
 import presetIcons from '@unocss/preset-icons'
+import { unheadVueComposablesImports } from '@unhead/vue'
+
 // import myAstroModule from './src/modules/my-module'
 
 // import { visualizer } from 'rollup-plugin-visualizer'
 
 import siteMeta from './src/site'
+// Increasing the maxListeners from default 10 to 15 as there are more than 10 vite plugins (mostly vite/astro) ones using fswatcher
+EventEmitter.defaultMaxListeners = 15
 const { url } = siteMeta
 
 // https://astro.build/config
@@ -71,8 +73,8 @@ export default defineConfig({
         // 'vue-router',
         // 'vue-i18n',
         // 'vue/macros',
-        // '@vueuse/head',
-        // '@vueuse/core',
+        unheadVueComposablesImports,
+        '@vueuse/core',
         'pinia',
       ],
       dirs: [
@@ -141,6 +143,10 @@ export default defineConfig({
         },
       }),
     ],
+
+    ssr: {
+      noExternal: ['primevue'],
+    },
 
     // build: {
     //   // Vite uses Rollup under the hold, so rollup options & plugins can be used for advanced usage
--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,11 @@ import 'uno.css'
 
 import type { App } from 'vue'
 import pinia from '@/plugins/pinia'
+import head from '@/plugins/head'
 import { primeVuePlugin, primeVueConfig } from '@/plugins/primevue'
 
 export default (app: App) => {
   app.use(pinia)
+  app.use(head)
   app.use(primeVuePlugin, primeVueConfig)
 }
